a05c418393355c3166b6c7715aa4c6f10a3f8f2c
GordanMasic <gordan.masic@BitCamps-Mac-mini.local>
2015-07-07 13:39:38 +0200

    FINAL version

--- Game/game/src/game/Window.java
+++ Game/game/src/game/Window.java
@@ -1,7 +1,8 @@
 package game;
 
-import java.awt.BorderLayout;
+
 import java.io.InputStream;
+
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JFrame;
@@ -12,33 +13,45 @@ public class Window extends JFrame {
 	private static final long serialVersionUID = -1270370868129612418L;
 
 	private JPanel mainPanel = new MyPanel();
+	InputStream ocean;
 
 	public Window() {
-
+		//Loading sound from file
+		ocean = ResourceLoader.load("ocean-waves-1.wav");
+		
+		
 		// Making and adding panel to widow
-		setLayout(new BorderLayout());
 		add(mainPanel);
 
+		
 		/*
 		 * Window default settings
 		 */
-		setTitle("Stick Hero");
+		setTitle("Pengin escape");
 		setSize(500, 500);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
 		setVisible(true);
-
+		while(true){
+		ocean = ResourceLoader.load("ocean-waves-1.wav");
+		playSound(ocean);
+		}
 	}
 
-
+	/**
+	 * Method for playing sound
+	 * 
+	 * @param sound
+	 *            sound loaded from file
+	 */
 	public static void playSound(InputStream sound) {
 		try {
 			Clip clip = AudioSystem.getClip();
 			clip.open(AudioSystem.getAudioInputStream(sound));
 			clip.start();
 
-			Thread.sleep(clip.getMicrosecondLength() / 1000);
+			Thread.sleep(clip.getMicrosecondLength());
 		} catch (Exception e) {
 
 		}
